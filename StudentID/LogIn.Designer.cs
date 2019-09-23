72c795ce59e13aa7a0220174f45372c47f50fbce
Devilgod18 <54558137+Devilgod18@users.noreply.github.com>
2019-09-22 23:06:05 -0500

    StudentID

---
+++ StudentID/LogIn.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace StudentLogin
+{
+    partial class LogIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.usersbox = new System.Windows.Forms.TextBox();
+            this.passbox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // usersbox
+            // 
+            this.usersbox.Location = new System.Drawing.Point(112, 25);
+            this.usersbox.Name = "usersbox";
+            this.usersbox.Size = new System.Drawing.Size(259, 22);
+            this.usersbox.TabIndex = 2;
+            // 
+            // passbox
+            // 
+            this.passbox.Location = new System.Drawing.Point(112, 69);
+            this.passbox.Name = "passbox";
+            this.passbox.Size = new System.Drawing.Size(259, 22);
+            this.passbox.TabIndex = 3;
+            this.passbox.UseSystemPasswordChar = true;
+            
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(182, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Sign In";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // LogIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 163);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.passbox);
+            this.Controls.Add(this.usersbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "LogIn";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.LogIn_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox usersbox;
+        private System.Windows.Forms.TextBox passbox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ StudentID/LogIn.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace StudentLogin
+{
+    public partial class LogIn : Form
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+            
+        }
+        List<string> users = new List<string>();
+        List<string> pass = new List<string>();
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (users.Contains(usersbox.Text) && pass.Contains(passbox.Text) && Array.IndexOf(users.ToArray(), usersbox.Text)
+                == Array.IndexOf(pass.ToArray(), passbox.Text))
+            {
+                Student stu = new Student();
+                stu.ShowDialog();
+            }
+            else
+                MessageBox.Show("Incorrect username/password");
+        }
+
+        private void LogIn_Load(object sender, EventArgs e)
+        {
+            StreamReader str = new StreamReader("ID.txt");
+            string line = "";
+            while ((line = str.ReadLine()) != null)
+            {
+                string[] components = line.Split("".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                users.Add(components[0]);
+                pass.Add(components[1]);
+              
+                
+            }
+            str.Close();
+        }
+
+        
+    }
+}
